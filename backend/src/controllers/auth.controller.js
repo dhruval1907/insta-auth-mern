0ed9c10965b01201e729b63f7f71678e33598730
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-12 10:57:06 +0530

    doneeeeeeeeeee

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -29,11 +29,48 @@ async function registerUser(req, res) {
     })
 }
 
-async function loginUser(req, res) {
-    const { email, password } = req.body
+async function loginUser (req, res) {
+    const { username, password, email } = req.body
+
+    const user = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    })
+
+    if (!user) {
+        return res.status(404).json({
+            message: "user not found"
+        })
+    }
+
+    const hash = crypto.createHash("sha256").update(password).digest("hex")
+
+    const isPasswordMatched = hash == user.password
+
+    if (!isPasswordMatched) {
+        return res.status(401).json({
+            message: "unauthorized access "
+        })
+    }
+
+    res.status(200).json({
+        message: "user logged in",
+        user: {
+            emial: user.email,
+            password: user.password,
+            username: user.username
+        }
+    })
+
 }
 
-module.exports = { registerUser }
+
+module.exports = {
+    registerUser,
+    loginUser
+}
 
 
 
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,47 +1,10 @@
 const express = require("express")
 const authRouter = express.Router()
 const authController = require("../controllers/auth.controller")
-const userModel = require("../models/user.model")
-const crypto = require("crypto")
-const e = require("express")
+// const userModel = require("../models/user.model")
 
 authRouter.post("/register", authController.registerUser)
-authRouter.post("/login", async (req, res) => {
-    const { username, password, email } = req.body
-
-    const user = await userModel.findOne({
-        $or: [
-            { username },
-            { email }
-        ]
-    })
-
-    if (!user) {
-        return res.status(404).json({
-            message: "user not found"
-        })
-    }
-
-    const hash = crypto.createHash("sha256").update(password).digest("hex")
-
-    const isPasswordMatched = hash == user.password
-
-    if (!isPasswordMatched) {
-        return res.status(401).json({
-            message: "unauthorized access "
-        })
-    }
-
-    res.status(200).json({
-        message: "user logged in",
-        user: {
-            emial: user.email,
-            password: user.password,
-            username: user.username
-        }
-    })
-
-})
+authRouter.post("/login", authController.loginUser)
 
 
 
