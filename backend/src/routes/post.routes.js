28eb6d6addfa8d2aaad9be1cbf240b49473ddd28
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-23 13:53:26 +0530

    follow part

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -12,12 +12,3 @@ postRouter.get("/posts", identifyUser, postController.getUserPost)
 postRouter.get("/posts/:postId", identifyUser, postController.getAllDataPost)
 
 module.exports = postRouter
-
-// const express = require("express")
-// const postRouter = express.Router()
-// const postController = require("../controllers/post.controller")
-// const multer = require("multer")
-// const upload = multer({ storage: multer.memoryStorage() })
-
-// postRouter.post("/", upload.single("image"), postController.createPostController)
-
