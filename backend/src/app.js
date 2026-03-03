756ff467ac8a6cb1fc66219a3fdc64bce2340e30
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-03-03 22:45:45 +0530

    shitttttttttty code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,20 @@
 require("dotenv").config()
 const express = require('express');
-const postRouter = require("../src/routes/post.routes")
 const cookieParser = require("cookie-parser");
-const authRouter = require("../src/routes/auth.routes");
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+
+// required routes
+const postRouter = require("../src/routes/post.routes")
+const authRouter = require("../src/routes/auth.routes");
+const userRouter = require("../src/routes/user.routes")
+
+// using routes
 app.use("/api/auth", authRouter)
 app.use("/api/post", postRouter)
+app.use("/api/user", userRouter)
 
 
 module.exports = app
--- backend/src/models/follow.model.js
+++ backend/src/models/follow.model.js
@@ -1,1 +1,19 @@
-const 
+const mongoose = require("mongoose")
+
+const followSchema = mongoose.Schema({
+    followers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+    }],
+    following: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+    }]
+}, {
+    timestamps: true
+})
+
+
+const followModel = mongoose.model("follow", followSchema)
